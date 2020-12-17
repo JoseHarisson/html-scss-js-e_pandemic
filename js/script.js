535cb5e535a330e248efd9692874f7be7121c201
JoseBowMan <razafimanantsoajosharisson@gmail.com>
2020-12-17 09:36:38 +0100

    js work... SUCCESS #question

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 const questionItems=document.querySelectorAll(".q-item"),
-      totalQuestionItems = questionItems.length; 
+      totalQuestionItems = questionItems.length, 
+      answerItems=document.querySelectorAll(".q-answer"),
+      totalAnswerItems = answerItems.length; 
       for(let i=0; i<totalQuestionItems;i++){ 
-          //console.log(questionItems[i]);
-        questionItems[i].addEventListener("click",function(){
-           /* for(let j=0; j<totalNavList;j++){
-                navList[j].querySelector("a").classList.remove("active");
-            }
-
-            this.classList.add("active");*/ 
-            console.log((questionItems[i].children[1]).getAttribute("id"));   
-        })
-    }
+          //console.log(questionItems[i]); 
+            console.log(questionItems[i]); 
+            questionItems[i].addEventListener("click",function(){  
+                if(answerItems[i].classList.contains("visible")){ 
+                    for(let i=0; i<totalAnswerItems;i++){
+                    if(answerItems[i].classList.contains("visible")){ 
+                        answerItems[i].classList.remove("visible");
+                    }
+                }
+                }
+                else{ 
+                    for(let i=0; i<totalAnswerItems;i++){
+                        if(answerItems[i].classList.contains("visible")){ 
+                            answerItems[i].classList.remove("visible");
+                        }
+                    }
+                    answerItems[i].classList.add("visible");  
+                }
+                 
+            }) 
+        } 
