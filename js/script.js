2d5a4adead2ddcb0e9313ca31db6e9f80c0c3a50
JoseBowMan <razafimanantsoajosharisson@gmail.com>
2020-12-17 09:51:17 +0100

    100% SUCCESS #question

--- js/script.js
+++ js/script.js
@@ -2,23 +2,24 @@ const questionItems=document.querySelectorAll(".q-item"),
       totalQuestionItems = questionItems.length, 
       answerItems=document.querySelectorAll(".q-answer"),
       totalAnswerItems = answerItems.length; 
+
+      function RemoveVisibility(){
+        for(let i=0; i<totalAnswerItems;i++){
+            if(answerItems[i].classList.contains("visible")){ 
+                answerItems[i].classList.remove("visible");
+            }
+        }
+      }
+      
       for(let i=0; i<totalQuestionItems;i++){ 
           //console.log(questionItems[i]); 
             console.log(questionItems[i]); 
             questionItems[i].addEventListener("click",function(){  
-                if(answerItems[i].classList.contains("visible")){ 
-                    for(let i=0; i<totalAnswerItems;i++){
-                    if(answerItems[i].classList.contains("visible")){ 
-                        answerItems[i].classList.remove("visible");
-                    }
-                }
+                if(answerItems[i].classList.contains("visible")){  
+                    RemoveVisibility(); 
                 }
                 else{ 
-                    for(let i=0; i<totalAnswerItems;i++){
-                        if(answerItems[i].classList.contains("visible")){ 
-                            answerItems[i].classList.remove("visible");
-                        }
-                    }
+                    RemoveVisibility();
                     answerItems[i].classList.add("visible");  
                 }
                  
